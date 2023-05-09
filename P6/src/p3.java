92d126a0ded0c538c558147b942d7e1d480ee53c
Esteban Agüero <cuentauniversidading@gmail.com>
2023-05-08 22:04:19 -0300

    nuevo commit

---
+++ P6/src/p3.java
@@ -0,0 +1,10 @@
+//Get into char abdc lower and convert to upper each letter
+public class p3 {
+    public static void main(String[] args)
+    {
+        String letras = "aBcDeFgH";
+        System.out.println("Letras sin modificar: "+letras);
+        System.out.println("Letras en minusculas: "+letras.toLowerCase());
+        System.out.println("Letras en mayusculas: "+letras.toUpperCase());
+    }
+}
